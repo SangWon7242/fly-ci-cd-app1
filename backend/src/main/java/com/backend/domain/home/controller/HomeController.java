f3de1e05d9890c465519f25c54a577dd78dfc5f2
SangWon7242 <gotkdqja@gmail.com>
2025-12-27 10:50:19 +0900

    work

--- backend/src/main/java/com/backend/domain/home/controller/HomeController.java
+++ backend/src/main/java/com/backend/domain/home/controller/HomeController.java
@@ -1,5 +1,6 @@
 package com.backend.domain.home.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,6 +8,7 @@ import java.net.InetAddress;
 import java.net.UnknownHostException;
 
 @RestController // @ResponseBody + @Controller
+@Slf4j
 public class HomeController {
   @GetMapping("/")
   public String main() {
@@ -17,6 +19,10 @@ public class HomeController {
     } catch (UnknownHostException e) {
       throw new RuntimeException(e);
     }
+
+    System.out.println("모든 환경에서 출력");
+    // log.info("개발/운영 환경에서만 출력");
+    log.debug("개발/테스트 환경에서만 출력");
 
     return "main1, hostname : %s".formatted(localhost.getHostName());
   }
